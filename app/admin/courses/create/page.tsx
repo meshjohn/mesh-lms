6d1a0008b174038781554bb5a6d06bc6a3c12d4b
meshjohn <meshjohn1@email.com>
2025-07-11 02:13:00 +0300

    add loading stream and finish course creation

--- app/admin/courses/create/page.tsx
+++ app/admin/courses/create/page.tsx
@@ -44,10 +44,14 @@ import { tryCatch } from "@/hooks/try-catch";
 import { CreateCourse } from "./actions";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { useConfetti } from "@/hooks/use-confetti";
 
 export default function CourseCreationPage() {
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
+
+  const { triggerConfetti } = useConfetti()
+
   // 1. Define your form
   const form = useForm<CourseSchemaType>({
     resolver: zodResolver(courseSchema),
@@ -73,6 +77,7 @@ export default function CourseCreationPage() {
       }
       if (result.status === "success") {
         toast.success(result.message);
+        triggerConfetti();
         form.reset();
         router.push("/admin/courses");
       } else if (result.status === "error") {
---
+++ hooks/use-confetti.ts
@@ -0,0 +1,38 @@
+"use client"
+
+import confetti from "canvas-confetti";
+
+export const useConfetti = () => {
+  const triggerConfetti = () => {
+    const count = 200;
+    const defaults = {
+      origin: { y: 0.7 },
+    };
+    function fire(particleRatio: number, opts: Record<string, unknown>) {
+      confetti({
+        ...defaults,
+        ...opts,
+        particleCount: Math.floor(count * particleRatio),
+      });
+    }
+    fire(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+    fire(0.2, {
+      spread: 60,
+      scalar: 0.8,
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
+  };
+  return { triggerConfetti };
+};
