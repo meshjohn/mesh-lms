5e4f7479cb29715924c1dbbeb3e05be61a7df988
meshjohn <meshjohn1@email.com>
2025-07-18 22:53:05 +0300

    next config update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 //@ts-ignore
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -12,12 +10,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-    return config;
-  },
+
 };
 
 export default nextConfig;
